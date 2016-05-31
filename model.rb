7535c0a8957b0e5fb70f38d0922991ae95a31145
Tom May <tom@tommay.net>
2016-05-31 11:49:24 -0700

    Split filename into filename + directory.  Add more usage examples.

--- model.rb
+++ model.rb
@@ -18,7 +18,8 @@ class Photo
   include DataMapper::Resource
 
   property :id,         Serial   # An auto-increment integer key
-  property :filename, String, length: 4000, required: true, unique: true
+  property :directory, String, length: 500, required: true, unique_index: :name
+  property :filename, String, length: 500, required: true, unique_index: :name
   property :filedate, DateTime, required: true       # Date file was modified.
   property :created_at, DateTime, required: true # Date this row was updated.
 
@@ -39,12 +40,55 @@ DataMapper.finalize
 DataMapper.auto_migrate!
 #DataMapper.auto_upgrade!
 
+Tag.create(
+  tag: "flower"
+).save
+tag = Tag.first(tag: "flower")
+
 photo = Photo.create(
-  filename: "/photo/one",
+  directory: "/photo",
+  filename: "one",
   filedate: Time.now,
   created_at: Time.now
 )
+photo.save
+photo.tags << tag
+photo.save
 
-tag = Tag.create(
-  tag: "flower"
+photo = Photo.create(
+  directory: "/photo",
+  filename: "two",
+  filedate: Time.now,
+  created_at: Time.now
 )
+photo.save
+photo.tags << tag
+photo.save
+
+photo.tags
+
+Photo.all(directory: "/photo").tags
+# SELECT "tags"."id", "tags"."tag"
+#   FROM "tags"
+#   INNER JOIN "photo_tags"
+#     ON "tags"."id" = "photo_tags"."tag_id"
+#   INNER JOIN "photos"
+#     ON "photo_tags"."photo_id" = "photos"."id"
+#   WHERE "photo_tags"."photo_id" IN
+#    (SELECT "id" FROM "photos" WHERE "directory" = '/photo')
+#   GROUP BY "tags"."id", "tags"."tag"
+#   ORDER BY "tags"."id"
+
+# SELECT tags.id, tags.tag
+#   FROM tags
+#   JOIN photo_tags
+#     ON photo_tags.tag_id = tags.id
+#   JOIN photos
+#     ON photos.id = photo_tags.photo_id
+#   WHERE photo_tags.photo_id IN
+#    (SELECT id FROM photos WHERE directory = '/photo')
+#   GROUP BY tags.id, tags.tag
+#   ORDER BY tags.id
+
+
+
