1137177574e2fecc8c84bf11d1fb37771e6f9d7a
Tom May <tom@tommay.net>
2016-06-01 01:06:28 -0700

    Change filename to basename.  Add Photo methods.

--- model.rb
+++ model.rb
@@ -19,11 +19,30 @@ class Photo
 
   property :id, Serial
   property :directory, String, length: 500, required: true, unique_index: :name
-  property :filename, String, length: 500, required: true, unique_index: :name
+  property :basename, String, length: 500, required: true, unique_index: :name
   property :filedate, DateTime, required: true # Date file was modified.
   property :created_at, DateTime, required: true # Date this row was updated.
 
   has n, :tags, through: Resource
+
+  def self.find_or_create(filename)
+    absolute = File.absolute_path(filename)
+    directory = File.dirname(absolute)
+    basename = File.basename(absolute)
+
+    photo = first(directory: directory, basename: basename)
+    if !photo
+      filedate = File.mtime(absolute)
+      photo = Photo.create(directory: directory, basename: basename,
+                           filedate: filedate, created_at: Time.now)
+      photo.save
+    end
+    photo
+  end
+
+  def filename
+    File.join(directory, basename)
+  end
 end
 
 class Tag
