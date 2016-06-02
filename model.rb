73f1f98ba1edfc6bc2272d30e8ce61b94f514c01
Tom May <tom@tommay.net>
2016-06-02 09:04:46 -0700

    Remove commented out in-memory Sqlite3 connection.

--- model.rb
+++ model.rb
@@ -6,9 +6,6 @@ require "byebug"
 # If you want the logs displayed you have to do this before the call to setup
 DataMapper::Logger.new($stdout, :debug)
 
-# An in-memory Sqlite3 connection:
-#DataMapper.setup(:default, "sqlite::memory:")
-
 # A Sqlite3 connection to a persistent database
 DataMapper.setup(:default, "sqlite:///home/tom/viewer/tags.db")
 
