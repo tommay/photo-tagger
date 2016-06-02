d3edd961686dfa1348d1f2f9826898a495c6f657
Tom May <tom@tommay.net>
2016-06-02 10:23:17 -0700

    Add tag,rb, command line tool to add and remove tags.

---
+++ tag.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+require "bundler/setup"
+require "optparse"
+require "byebug"
+
+require_relative "model"
+
+add = []
+remove = []
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: $0 [options] [filename...]"
+
+  opts.on("-a", "--add TAG", "Add TAG to files") do |tag|
+    add << tag
+  end
+
+  opts.on("-r", "--remove TAG", "Remove TAG from files") do |tag|
+    remove << tag
+  end
+
+  opts.on("-h", "--help", "Print this help") do
+    puts opts
+    exit
+  end
+end.parse!
+
+ARGV.each do |filename|
+  photo = Photo.find_or_create(filename)
+
+  add.each do |tag|
+    photo.add_tag(tag)
+  end
+
+  remove.each do |tag|
+    photo.remove_tag(tag)
+  end
+end
