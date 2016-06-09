e9ba12f063b03079dbcfd8d5cd416f24e7d20a43
Tom May <tom@tommay.net>
2016-06-09 08:57:18 -0700

    Add -t to list tags.

--- cli/list.rb
+++ cli/list.rb
@@ -8,11 +8,14 @@ require_relative "../model"
 # list [-d|-f]
 #   -f list files in the database
 #   -d list directories in the database
+#   -t list tags
 
 options = Trollop::options do
   banner "Usage: #{$0} [options]"
   opt :files, "List files in the database"
   opt :directories, "List directories in the database"
+  opt :tags, "List tags in the database"
+  conflicts :files, :directories, :tags
 end
 
 case
@@ -23,5 +26,9 @@ when options.files
 when options.directories
   Photo.all(:fields => [:directory], :unique => true, :order => [:directory.asc]).each do |photo|
     puts "#{photo.directory}"
+  end
+when options.tags
+  Tag.all.map{|t|t.tag}.sort.each do |tag|
+    puts "#{tag}"
   end
 end
