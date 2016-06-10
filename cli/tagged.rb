7816d1f962af04d418adcca452fec64786f1ddc6
Tom May <tom@tommay.net>
2016-06-09 18:26:30 -0700

    Add -t switch to display files' tags.

--- cli/tagged.rb
+++ cli/tagged.rb
@@ -5,9 +5,11 @@ require "trollop"
 require "byebug"
 require_relative "../model"
 
-#options = Trollop::options do
-#  banner "Usage: #{$0} tag..."
-#end
+options = Trollop::options do
+  banner "Usage: #{$0} tag..."
+  opt :tags, "Show files' tags"
+  stop_on_unknown
+end
 
 photos = ARGV.map do |tag|
   tag =~ /^(-?)(.*)/
@@ -22,10 +24,22 @@ _, photos = photos.reduce do |(_, memo), (type, photos)|
   end
 end
 
-filenames = photos.map do |photo|
-  photo.filename
-end
+if options.tags
+  photos = photos.map do |photo|
+    [photo.filename, photo.tags.map{|t|t.tag}.sort]
+  end
 
-filenames.sort.each do |filename|
-  print "#{filename}\0"
+  photos = photos.sort_by{|p| p[0]}
+
+  photos.each do |photo|
+    puts "#{photo[0]}: #{photo[1].join(", ")}"
+  end
+else
+  filenames = photos.map do |photo|
+    photo.filename
+  end
+
+  filenames.sort.each do |filename|
+    print "#{filename}\0"
+  end
 end
