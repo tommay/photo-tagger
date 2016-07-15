3491005da25a59d52ab8c71bb3716bc6c6a3f4ac
Tom May <tom@tommay.net>
2016-07-15 15:30:28 -0700

    Filenames are sorted so that numeric substrings sort in numeric order, so "spud_9.jpg" sorts before "spud_10.jpg".

--- files.rb
+++ files.rb
@@ -1,3 +1,5 @@
+require "strscan"
+
 module Files
   SUFFIXES = ["jpg", "png"].map do |suffix|
     %r{\.#{suffix}$}i
@@ -12,7 +14,7 @@ module Files
   def self.for_directory(dirname)
     Dir[dirname ? File.join(dirname, "*") : "*"].select do |filename|
       image_file?(filename)
-    end.sort
+    end.sort_by{|name| name_split(name)}
   end
 
   def self.image_files(filename, recurse)
@@ -31,6 +33,27 @@ module Files
       end
     when image_file?(filename)
       yielder << filename
+    end
+  end
+
+  # Split a string into an Array of alternating non-numerics and
+  # numerics.  The arrays can then be compared, and numbers will sort
+  # in numeric order instead of alphabetic, e.g., "spud_9" sorts
+  # before "spud_10".  "spud_1" compares equal to "spud_01" but that
+  # shouldn't be a problem.  But if it is, equal numbers are now
+  # ordered by the length of their generating string. so that shorter
+  # strings sort first.
+  #
+  def self.name_split(string)
+    scanner = StringScanner.new(string)
+    [].tap do |result|
+      while !scanner.eos?
+        result << scanner.scan(%r{([^0-9]*)})
+        sub = scanner.scan(%r{([0-9]+)})
+        if sub
+          result << [sub.to_i, sub.size]
+        end
+      end
     end
   end
 end
