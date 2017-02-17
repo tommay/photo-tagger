4c86052ad851ff1c169658db2d9cab3a1fc03c12
Tom May <tom@tommay.net>
2017-02-17 10:26:08 -0800

    Better fix for list blowing out on EPIPE.

--- cli/list.rb
+++ cli/list.rb
@@ -20,39 +20,48 @@ EOS
   opt :created, "Order files/directories by (database) creation time, most recent first"
 end
 
-begin
-  case
-  when options.files
-    if options.created
-      Photo.order(Sequel.desc(:created_at)).each do |photo|
-        puts photo.filename
-      end
-    else
-      Photo.map do |photo|
-        photo.filename
-      end.sort.each do |filename|
-        puts filename
-      end
+# If the STDOUT pipe is closed, either via ^C or something simple like piping
+# into head, then puts will raise Errno::EPIPE and if it's called in
+# a Sequel #each block then Sequel will handle it and barf all over the
+# place.  So just handle it here and exit relatively cleanly.
+
+def safe_puts(*args)
+  begin
+    puts(*args)
+  rescue Errno::EPIPE
+    exit(1)
+  end
+end
+
+case
+when options.files
+  if options.created
+    Photo.order(Sequel.desc(:created_at)).each do |photo|
+      safe_puts photo.filename
     end
-  when options.directories
-    if options.created
-      seen = {}
-      Photo.order(Sequel.desc(:created_at)).each do |photo|
-        if !seen[photo.directory]
-          puts photo.directory
-          seen[photo.directory] = true
-        end
-      end
-    else
-      Photo.distinct.select(:directory).order(:directory).each do |photo|
-        puts photo.directory
-      end
-    end
-  when options.tags
-    Tag.order(Sequel.desc(:created_at)).each do |tag|
-      puts "%-20s %s" % [tag.tag, tag.created_at]
+  else
+    Photo.map do |photo|
+      photo.filename
+    end.sort.each do |filename|
+      safe_puts filename
     end
   end
-rescue Errno::EPIPE => ex
-  # The program we were piping into got ^C'd or something, no worries.
+when options.directories
+  if options.created
+    seen = {}
+    Photo.order(Sequel.desc(:created_at)).each do |photo|
+      if !seen[photo.directory]
+        safe_puts photo.directory
+        seen[photo.directory] = true
+      end
+    end
+  else
+    Photo.distinct.select(:directory).order(:directory).each do |photo|
+      safe_puts photo.directory
+    end
+  end
+when options.tags
+  Tag.order(Sequel.desc(:created_at)).each do |tag|
+    safe_puts "%-20s %s" % [tag.tag, tag.created_at]
+  end
 end
