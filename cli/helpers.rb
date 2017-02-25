89c5bb99b8d682fc7adaab21ba457a9807b7a4d4
Tom May <tom@tommay.net>
2017-02-25 04:05:01 -0800

    helpers.rb overwrites puts and print on self/main to exit on EPIPE.

--- cli/helpers.rb
+++ cli/helpers.rb
@@ -1,5 +1,19 @@
 require_relative "../files"
 
+exit_on_epipe = lambda do |method|
+  original_method = method(method)
+  define_singleton_method(method) do |*args|
+    begin
+      original_method.call(*args)
+    rescue Errno::EPIPE
+      exit(1)
+    end
+  end
+end
+
+exit_on_epipe.call(:puts)
+exit_on_epipe.call(:print)
+
 def process_args(args, recurse, &block)
   args.each do |filename|
     Files.image_files(filename, recurse).each(&block)
